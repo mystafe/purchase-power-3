55535267f43c4c4c0a7a7f1c4e2532fa0a672533
mystafe <admess34@gmail.com>
2025-01-06 21:10:28 +0400

    ench3

--- src/utils/calculateValues.js
+++ src/utils/calculateValues.js
@@ -10,35 +10,76 @@ export const calculateValues = (data, baseCurrency, startDate, endDate, amount) 
     return { startValues: null, endValues: null };
   }
 
-  const startValues = {
-    tryValue: baseCurrency === "TRY" ? amount : amount * startData.USDTRY,
-    usdValue: baseCurrency === "USD" ? amount : amount / startData.USDTRY,
-    eurValue: amount / startData.EURTRY,
-    goldValue: amount / startData.GoldPerGramTRY,
-    minWageRatio: amount / startData.minWageNetTRY,
-    normalizedValue:
-      baseCurrency === "TRY"
-        ? amount / startData.USDTRY_TRY_NORM
-        : amount * startData.USDTRY_TRY_NORM,
-  };
+  // TRY ve USD Bazlı Hesaplama
+  const startTRY = baseCurrency === "TRY" ? amount : amount * startData.USDTRY;
+  const endTRY =
+    baseCurrency === "TRY"
+      ? startTRY * (endData.TRYInflationIndex / startData.TRYInflationIndex)
+      : startTRY * (startData.USDTRY / endData.USDTRY);
 
-  const endValues = {
-    tryValue:
-      baseCurrency === "TRY"
-        ? startValues.tryValue * (endData.TRYInflationIndex / startData.TRYInflationIndex)
-        : amount * (startData.USDTRY / endData.USDTRY),
-    usdValue:
-      baseCurrency === "USD"
-        ? startValues.usdValue * (endData.USDInflationIndex / startData.USDInflationIndex)
-        : amount / endData.USDTRY,
-    eurValue: startValues.eurValue * (startData.EURTRY / endData.EURTRY),
-    goldValue: startValues.goldValue * (startData.GoldPerGramTRY / endData.GoldPerGramTRY),
-    minWageRatio: amount / endData.minWageNetTRY,
-    normalizedValue:
-      baseCurrency === "TRY"
-        ? amount / endData.USDTRY_TRY_NORM
-        : amount * endData.USDTRY_TRY_NORM,
-  };
+  const startUSD = baseCurrency === "USD" ? amount : startTRY / startData.USDTRY;
+  const endUSD =
+    baseCurrency === "USD"
+      ? startUSD * (endData.USDInflationIndex / startData.USDInflationIndex)
+      : endTRY / endData.USDTRY;
 
-  return { startValues, endValues };
+  // EUR Hesaplama
+  const startEUR =
+    baseCurrency === "USD"
+      ? amount * (startData.USDTRY / startData.EURTRY)
+      : startTRY / startData.EURTRY;
+  const endEUR =
+    baseCurrency === "USD"
+      ? startEUR * (endData.USDTRY / endData.EURTRY)
+      : endTRY / endData.EURTRY;
+
+  // Altın Hesaplama
+  const startGold =
+    baseCurrency === "USD"
+      ? amount / (startData.GoldPerGramTRY / startData.USDTRY)
+      : startTRY / startData.GoldPerGramTRY;
+  const endGold =
+    baseCurrency === "USD"
+      ? startGold * (startData.GoldPerGramTRY / endData.GoldPerGramTRY)
+      : endTRY / endData.GoldPerGramTRY;
+
+  // Asgari Ücret Hesaplama
+  const startMinWageRatio =
+    baseCurrency === "USD"
+      ? amount / (startData.minWageNetTRY / startData.USDTRY)
+      : startTRY / startData.minWageNetTRY;
+  const endMinWageRatio =
+    baseCurrency === "USD"
+      ? endUSD / (endData.minWageNetTRY / endData.USDTRY)
+      : endTRY / endData.minWageNetTRY;
+
+  // Normalize Edilmiş Değer Hesaplama
+  const startNormalized =
+    baseCurrency === "TRY"
+      ? startTRY / startData.USDTRY_TRY_NORM
+      : startUSD * startData.USDTRY_TRY_NORM;
+  const endNormalized =
+    baseCurrency === "TRY"
+      ? endTRY / endData.USDTRY_TRY_NORM
+      : endUSD * endData.USDTRY_TRY_NORM;
+
+  // Değerleri Dönüş
+  return {
+    startValues: {
+      tryValue: startTRY,
+      usdValue: startUSD,
+      eurValue: startEUR,
+      goldValue: startGold,
+      minWageRatio: startMinWageRatio,
+      normalizedValue: startNormalized,
+    },
+    endValues: {
+      tryValue: endTRY,
+      usdValue: endUSD,
+      eurValue: endEUR,
+      goldValue: endGold,
+      minWageRatio: endMinWageRatio,
+      normalizedValue: endNormalized,
+    },
+  };
 };
