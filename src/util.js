69b1dc1a01257021fe28792972c4088ba61a1c11
Mustafa Evleksiz <75567558+mystafe@users.noreply.github.com>
2025-07-13 18:20:21 +0400

    Update util.js 18.20

--- src/util.js
+++ src/util.js
@@ -1,6 +1,3 @@
-
-
-
 const fs = require('fs');
 const path = require('path');
 
@@ -9,40 +6,28 @@ function formatUsdTryFromFile() {
   try {
     const rawData = fs.readFileSync(filePath, 'utf8');
     const data = JSON.parse(rawData);
-
     const formattedData = data.map(entry => ({
       ...entry,
       USDTRY: parseFloat(entry.USDTRY.toFixed(2)),
       EURTRY: parseFloat(entry?.EURTRY?.toFixed(2)) || 1,
     }));
-
     return formattedData;
   } catch (error) {
     console.error('Dosya okunamadı:', error.message);
     return [];
   }
 }
-
 // Örnek kullanım
 const formattedData = formatUsdTryFromFile();
-
-
 function printDataLineByLine(data) {
   data.forEach(item => {
     console.log(item.EURTRY.toFixed(2)); // Değeri iki ondalık haneye yuvarlayarak yazdırır
   });
 }
-
-
 // Çalıştırma
 printDataLineByLine(formattedData);
-
 function saveDataToFile(data, fileName) {
   fs.writeFileSync(fileName, JSON.stringify(data, null, 2), 'utf8');
   console.log(`Data saved to ${fileName}`);
 }
-
 saveDataToFile(formattedData, 'output.json');
-
-
-
